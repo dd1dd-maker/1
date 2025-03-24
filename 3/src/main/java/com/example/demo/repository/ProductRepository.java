39c7b9342fdf3efd70313bcb5adc02f9c34b3b77
dd1dd-maker <1009778175@qq.com>
2025-03-24 15:24:13 +0800

    Delete 3 directory

--- 3/src/main/java/com/example/demo/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-// 修改包声明以匹配文件路径
-package com.example.demo.repository;
-
-import com.example.demo.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
