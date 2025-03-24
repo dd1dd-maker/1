e5816d6b62a29e1148a0ea2692b39430340d286e
dd1dd-maker <1009778175@qq.com>
2025-03-24 15:25:25 +0800

    Add files via upload

---
+++ 3/src/main/java/com/example/demo/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+// 修改包声明以匹配文件路径
+package com.example.demo.repository;
+
+import com.example.demo.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
